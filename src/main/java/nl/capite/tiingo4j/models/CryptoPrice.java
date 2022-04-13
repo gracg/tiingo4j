14b4d8b458477d6385593e7e7b012465dd7e8986
gracg <grac@gracg.top>
2022-04-13 14:58:33 -0400

    new CryptoPrice and CryptoPriceData models.

---
+++ src/main/java/nl/capite/tiingo4j/models/CryptoPrice.java
@@ -0,0 +1,14 @@
+package nl.capite.tiingo4j.models;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class CryptoPrice {
+    private String ticker;
+    private String baseCurrency;
+    private String quoteCurrency;
+    private CryptoPriceData[] priceData;
+    private Map<String,CryptoTopOfTheBookDataProvider[]> exchangeData;
+}
