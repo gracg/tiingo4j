815a07dd9f100e711283c6614eddcc39f708238b
gracg <grac@gracg.top>
2022-04-13 14:57:29 -0400

    new CRYPTO_RESAMPLE_FREQUENCY enum.

---
+++ src/main/java/nl/capite/tiingo4j/enums/CRYPTO_RESAMPLE_FREQUENCY.java
@@ -0,0 +1,19 @@
+package nl.capite.tiingo4j.enums;
+
+public enum CRYPTO_RESAMPLE_FREQUENCY {
+    MINUTE("min"),
+    HOUR("hour"),
+    DAY("day");
+
+    private String value;
+
+    CRYPTO_RESAMPLE_FREQUENCY(String string) {
+        value = string;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+
+    }
+}
