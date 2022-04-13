aa2aa7c8e9a8bda18834c2f53ca4693be926a24d
gracg <grac@gracg.top>
2022-04-13 12:57:55 -0400

    New CryptoTopOfTheBookParameters file.

---
+++ src/main/java/nl/capite/tiingo4j/requestParameters/CryptoTopOfTheBookParameters.java
@@ -0,0 +1,24 @@
+package nl.capite.tiingo4j.requestParameters;
+
+import nl.capite.tiingo4j.abstracts.AbstractParameters;
+
+import java.util.List;
+
+public class CryptoTopOfTheBookParameters extends AbstractParameters {
+
+
+
+    public CryptoTopOfTheBookParameters setExchanges(List<String> exchanges) {
+        if(exchanges!=null && exchanges.size()>0) {
+            map.put("exchanges",csvString(exchanges));
+        }
+        return this;
+    }
+
+    public CryptoTopOfTheBookParameters setIncludeRawExchangeData(boolean b) {
+        map.put("includeRawExchangeData",String.valueOf(b));
+        return this;
+    }
+
+
+}
